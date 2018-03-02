10d06d3546550edde7526effc33415cc617da008
Tony <anthony.c.young18@gmail.com>
2018-03-02 16:14:01 +0000

    added city name to table

---
+++ src/containers/weatherList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends React.Component {
+
+  renderWeather(cityData) {
+
+    return (
+      <tr key={ cityData.city.name }>
+        <td>{ cityData.city.name }</td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temp</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+
+  return { weather: state.weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
