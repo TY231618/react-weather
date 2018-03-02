8802dc4b455d94b36573b6ce84fcb10c544ab77d
Tony <anthony.c.young18@gmail.com>
2018-03-02 16:30:16 +0000

    added charts

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends React.Component {
 
   renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
-      <tr key={ cityData.city.name }>
-        <td>{ cityData.city.name }</td>
+      <tr key={ name }>
+        <td>{ name }</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={pressures}>
+            <SparklinesLine color="green" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={humidities}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
       </tr>
     )
   }
