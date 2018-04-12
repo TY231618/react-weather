58fbfd7f3ff9f1ab6360567a4d58bcbfb69e6b04
Tony <anthony.c.young18@gmail.com>
2018-04-12 11:27:45 +0100

    added header component, changed default dev port to 8081 and imported materialize

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../containers/header/header';
 import SearchBar from '../containers/searchBar/search_bar_container';
 import WeatherList from '../containers/weatherList/weatherList_container';
 import RandomContent from '../containers/content/random_content_container';
@@ -8,9 +9,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <SearchBar />
         <WeatherList />
-        <MovieReviewList />
       </div>
     );
   }
---
+++ src/containers/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <h2 className="center">USA Weather App</h2>
+      </div>
+    </nav>
+  )
+};
+
+export default Header;
